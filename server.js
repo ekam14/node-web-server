40a5de74f696d2559a25f7b9b2a3983ae20e0d5c
Ekam Singh Chahal <ecsingh1997@gmail.com>
2018-06-16 23:23:29 +0530

    =Initial Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');   //to use partials i.e same code at different locations//
+//Tells us about the view engine of the template (template engine)
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  fs.appendFile('server.log',log+'\n',(err) =>{
+    if(err){
+      console.log('Unable to append to the server.log');
+    }
+  });
+  next(); //will hang here if next is not called//
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintainence.hbs');
+// });
+
+//Middleware to use the static public folder
+app.use(express.static(__dirname + '/public'));
+
+//Hbs helper functions ,used if we are sending same key-value pair as an argument
+hbs.registerHelper('getYear',() =>{
+  return new Date().getFullYear();
+});
+
+app.get('/',(req,res) =>{
+  res.render('home.hbs',{
+    message:"Welcome to the Home page of the app",
+    title:'Home Page'
+  });
+});
+
+app.get('/about',(req,res) =>{
+  res.render('about.hbs',{   //helps to call the templates in views folder
+    title:'About Page'
+  });
+});
+
+
+app.listen(3000,() => {
+  console.log('App started at port 3000');
+});
